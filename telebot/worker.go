f533cb775d2ccf941f508c0d484c7acbcb27a715
alexiskov <alexiskov.rus@gmail.com>
2024-11-14 12:34:42 +0300

    tg automatic job vacancy mailer rewrited

--- telebot/worker.go
+++ telebot/worker.go
@@ -2,43 +2,50 @@ package telebot
 
 import (
 	"context"
+	"time"
 	"vacancydealer/bd"
 	"vacancydealer/logger"
 
 	"github.com/go-telegram/bot"
 )
 
+// Automatic worker
+// New vacancieAnnounces to user sent
 func StartWorker(ctx context.Context, b *bot.Bot) {
-	uds, err := bd.GetAllUserData()
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-
-	for _, ud := range uds {
-		a, err := ud.GetJobAnnounces()
+	for {
+		uds, err := bd.GetAllUserData()
 		if err != nil {
 			logger.Error(err.Error())
-			continue
+			return
 		}
 
-		var showedJobAnnoucesIDs []uint
-
-		for _, ja := range convertJobDataModelDBtoTG(a) {
-			if err = ja.sentJobAnnounceToClient(ctx, ud.TgID, b); err != nil {
+		for _, ud := range uds {
+			a, err := ud.GetJobAnnounces()
+			if err != nil {
 				logger.Error(err.Error())
 				continue
 			}
 
-			showedJobAnnoucesIDs = append(showedJobAnnoucesIDs, ja.ItemID)
-		}
+			var showedJobAnnoucesIDs []uint
 
-		if len(showedJobAnnoucesIDs) != 0 {
-			if err = bd.CreatePivotVacancyAnnouncesAndUserIds(showedJobAnnoucesIDs, uint(ud.TgID)); err != nil {
-				logger.Error(err.Error())
-				continue
+			for _, ja := range convertJobDataModelDBtoTG(a) {
+				if err = ja.sentJobAnnounceToClient(ctx, ud.TgID, b); err != nil {
+					logger.Error(err.Error())
+					continue
+				}
+
+				showedJobAnnoucesIDs = append(showedJobAnnoucesIDs, ja.ItemID)
+			}
+
+			if len(showedJobAnnoucesIDs) != 0 {
+				if err = bd.CreatePivotVacancyAnnouncesAndUserIds(showedJobAnnoucesIDs, uint(ud.TgID)); err != nil {
+					logger.Error(err.Error())
+					continue
+				}
 			}
 		}
 
+		time.Sleep(time.Duration(10800/len(uds)) * time.Second) //period
 	}
+
 }
